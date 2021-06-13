4ea5e19924b79345d624f2928ef32db0835200a8
frezixs <49871539+frezixs@users.noreply.github.com>
2021-06-13 19:17:41 -0400

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+--Deliverable 1: The Number of Retiring Employees by Title (50 points)
 SELECT 
 	e.emp_no,
 	e.first_name,
@@ -5,7 +6,7 @@ SELECT
 	ti.title,
 	ti.from_date, 
 	ti.to_date 
-INTO retirement_titles
+--INTO retirement_titles
 FROM 
 employees as e
 INNER JOIN 
@@ -19,12 +20,14 @@ SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
 last_name,
 title
-INTO unique_titles
+--INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
+SELECT COUNT(*) FROM unique_titles 
+
 SELECT count(title),title
-INTO retiring_titles 
+--INTO retiring_titles 
 FROM unique_titles 
 GROUP BY title 
 ORDER BY count(title) DESC;
@@ -34,3 +37,77 @@ DROP TABLE retirement_titles;
 DROP TABLE unique_titles;
 
 SELECT * FROM retiring_titles
+
+--Deliverable 2: The Employees Eligible for the Mentorship Program (30 points)
+SELECT DISTINCT ON (emp_no)
+	e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date, 
+	de.to_date,
+	ti.title 
+INTO mentorship_eligibilty
+FROM 
+employees as e 
+INNER JOIN 
+dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN 
+titles as ti 
+ON (de.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no; 
+
+SELECT COUNT(*) FROM mentorship_eligibilty
+
+--Deliverable 3: A written report on the employee database analysis (20 points)
+SELECT 
+	e.emp_no,
+	e.first_name,
+	e.last_name, 
+	ti.title,
+	ti.from_date, 
+	ti.to_date
+INTO 
+employee_titles
+FROM 
+employees as e
+INNER JOIN 
+titles as ti
+ON (e.emp_no = ti.emp_no)
+ORDER BY e.emp_no;
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_employee_titles
+FROM employee_titles
+ORDER BY emp_no, to_date DESC;
+
+SELECT COUNT(*) FROM unique_employee_titles 
+
+
+SELECT DISTINCT ON (emp_no)
+	e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date, 
+	de.to_date,
+	ti.title 
+INTO current_em
+FROM 
+employees as e 
+INNER JOIN 
+dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN 
+titles as ti 
+ON (de.emp_no = ti.emp_no)
+WHERE (de.from_date > '1997-01-01')
+ORDER BY e.emp_no; 
+
+SELECT COUNT(*) FROM current_em
+
